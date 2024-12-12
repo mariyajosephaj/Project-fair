17daf6dc37bcc0eb3d37328795cfb5fab6e3d70f
mariyajosephaj <mariya96aj@gmail.com>
2024-12-12 11:17:58 +0530

    project fair completed

--- src/services/serverUrl.js
+++ src/services/serverUrl.js
@@ -1,2 +1,4 @@
-const SERVER_BASE_URL ="http://localhost:3000"
+// const SERVER_BASE_URL ="http://localhost:3000"
+// https://project-fair-server-wjo3.onrender.com/
+const SERVER_BASE_URL ="https://project-fair-server-wjo3.onrender.com"
 export default SERVER_BASE_URL
